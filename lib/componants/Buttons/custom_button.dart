ee77d23a99faefcf311ef052758cfad02269232a
karimelsherbinii <70584116+karimelsherbinii@users.noreply.github.com>
2022-05-12 11:29:33 +0200

    add all

---
+++ lib/componants/Buttons/custom_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomRoundedButton extends StatelessWidget {
+  CustomRoundedButton({
+    Key? key,
+    this.width,
+    this.height,
+    this.text,
+    this.buttonColor,
+    this.fontWeight,
+    this.fontSize,
+    this.fontColor,
+    this.radiusCount,
+  }) : super(key: key);
+  double? width;
+  double? height;
+  String? text;
+  Color? buttonColor;
+  FontWeight? fontWeight;
+  double? fontSize;
+  Color? fontColor;
+  double? radiusCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // go to ...
+      },
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+            color: buttonColor,
+            border: Border.all(width: 0.5, color: Colors.grey),
+            borderRadius: BorderRadius.circular(radiusCount!)),
+        child: Center(
+          child: Text(
+            text!,
+            style: TextStyle(
+              color: fontColor,
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
